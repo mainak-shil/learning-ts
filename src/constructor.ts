4036eba94a3681fe54a0d9b9597094c40379aba2
Mainak <mainakshil.1@gmail.com>
2022-06-03 01:14:17 +0530

    constructor

---
+++ src/constructor.ts
@@ -0,0 +1,20 @@
+class Department {
+  name: string;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
+}
+
+const accounting = new Department("Accounting");
+
+accounting.describe();
+
+// fn pass
+const accountingCopy = { name: "DUMMY", describe: accounting.describe };
+
+accountingCopy.describe();
