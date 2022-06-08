3cfc5f716a9458cde932b4a165a05eb5a880da2c
Mainak <mainakshil.1@gmail.com>
2022-06-09 00:46:52 +0530

    static-nonstatic

---
+++ dist/static-nonstatic.js
@@ -0,0 +1,11 @@
+"use strict";
+class Circle {
+    constructor() {
+        this.pi = 3;
+    }
+}
+Circle.pi = 3.14;
+Circle.pi; // returns 3.14
+let circleObj = new Circle();
+circleObj.pi; // returns 3
+//# sourceMappingURL=static-nonstatic.js.map
---
+++ src/static-nonstatic.ts
@@ -0,0 +1,9 @@
+class Circle {
+  static pi = 3.14;
+  pi = 3;
+}
+
+Circle.pi; // returns 3.14
+
+let circleObj = new Circle();
+circleObj.pi; // returns 3
